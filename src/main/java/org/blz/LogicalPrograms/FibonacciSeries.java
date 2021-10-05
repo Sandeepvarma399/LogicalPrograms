638ba3f6e0b63e95d6481db8035bb6e0f8f239c3
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-05 08:59:27 +0530

    Added:Logical Programs

---
+++ src/main/java/org/blz/LogicalPrograms/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package org.blz.LogicalPrograms;
+
+import java.util.Scanner;
+
+
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        int a = 0, b = 1, c;
+        for (int i = 1; i <= num; i++) {
+            c = a + b;
+            System.out.print(c + ", ");
+            a = b;
+            b = c;
+        }
+
+    }
+}
